7e221cb62b17a5c90760c0a2ade5f18f30b58173
Bea <bea4freelance@gmail.com>
2024-07-01 10:45:47 +0800

    Update screenshot handling for user activity

--- backend/app/controllers/activityController.js
+++ backend/app/controllers/activityController.js
@@ -1,4 +1,6 @@
 const model = require('../models/activityModel');
+const path = require('path');
+const fs = require('fs');
 class ActivityController {
 
     static async addEntry(id){
@@ -9,10 +11,14 @@ class ActivityController {
         }
     }
 
-    static async getDailyActivity(req, res, id){
+    static async getDailyActivity(id){
         try{
             const activity = await model.getDailyActivity(id);
-            res.status(200).json({activity: activity[0].activity});
+            if(activity.length>0){
+                return activity[0].activity;
+            }else{
+                return 0;
+            }
         }catch(error){
             console.error(error, 'Unable to retrieve tracked hours for this project.')
         }
@@ -25,6 +31,43 @@ class ActivityController {
             await model.saveActivity(user_id, activity);
         }catch(error){
             console.error(error,'Unable to save tracked hours for this project.');
+        }
+    }
+
+    static async saveScreenShot(req, res, id){
+        try{
+            const { screenshot, fileName, time } = req.body;
+            const imgBuffer = Buffer.from(screenshot, 'base64');
+            const directory = path.join(__dirname, '../screenshots');
+
+            if (!fs.existsSync(directory)) {
+                fs.mkdirSync(directory, { recursive: true });
+            }
+
+            const filePath = path.join(directory, fileName);
+
+            fs.writeFileSync(filePath, imgBuffer);
+            await model.saveScreenShot(id, filePath, time);
+            res.sendStatus(200);
+        }catch(error){
+            console.error(error, 'Unable to save screen capture at this time.')
+        }
+    }
+    
+    static async getScreenShots(id){
+        try{
+            const response = await model.getScreenShots(id)
+            if(response && response[0].images ){
+                const imageUrls = JSON.parse(response[0].images).map(file => {
+                const parsedFile = JSON.parse(file);
+                return {filePath: path.basename(parsedFile.filePath), time:parsedFile.time};
+                });
+                return imageUrls;
+            }else{
+                return {};
+            }
+        }catch(error){
+            console.error(error, 'Unable to fetch screen captures at this time.')
         }
     }
 }
