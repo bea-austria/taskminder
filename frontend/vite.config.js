d4fb0700b2886f3412207b964aa315c8e3dc1edc
Bea <bea4freelance@gmail.com>
2024-07-04 17:39:01 +0800

    Build output for react-vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: 'src/renderer',
+  build: {
+    outDir: '../../dist', // Adjust this path as needed
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:5000'
